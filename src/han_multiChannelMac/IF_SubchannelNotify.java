1f760ab1169396548d8a23fe69f8ddee0106f531
ycqfeng <ycqfeng@vip.qq.com>
2017-01-03 14:13:40 +0800

    构建框架

--- src/han_multiChannelMac/IF_SubchannelNotify.java
+++
@@ -1,9 +0,0 @@
-package han_multiChannelMac;
-
-/**
- * Created by ycqfeng on 2017/1/2.
- */
-public interface IF_SubchannelNotify {
-    void beOccupied(SubChannel subChannel);
-    void beReleased(SubChannel subChannel);
-}
--- src/han_multiChannelMac/PacketType.java
+++
@@ -1,9 +0,0 @@
-package han_multiChannelMac;
-
-/**
- * Created by ycqfeng on 2017/1/3.
- */
-public enum PacketType {
-    RTS,
-    OTHER
-}
